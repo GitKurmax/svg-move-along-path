d742a0fc6832d879c0a872d045374dffdaef53cd
Alexandr <alexandrmail32@gmail.com>
2020-08-07 23:33:02 +0300

    Added plane fly

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
 let plane = document.querySelector('.plane');
-// let pathCoords = `M0 0 500 200 600 700 L${document.documentElement.clientWidth} ${document.documentElement.clientHeight}`;
-pathCoords = `M0 0 500 200  L600 700`;
-
-
+let pathCoords = 
+    `M0 0 C 40 10, 165 10, 450 280 S 700 250, ${document.documentElement.clientWidth} ${document.documentElement.clientHeight}`;
 path.setAttribute('d', pathCoords);
-path.setAttribute('fill', 'none');
-path.setAttribute('stroke', '#000000');
-path.setAttribute('stroke-width', 2);
-path.setAttribute('id', 'path');
-path.setAttribute('d', pathCoords);
-// plane.style.offsetPath = `path(${pathCoords})`;
+plane.style.offsetPath = `path('${pathCoords}')`;
 document.getElementsByTagName('svg')[0].appendChild(path);
 
 
