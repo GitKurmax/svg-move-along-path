f30881f2032ee74f04e67329b931d8087828b834
alex <alexandrmail32@gmail.com>
2020-08-07 18:22:32 +0300

    Added animation

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-let pathCoords = `M0 0 500 200 600 700 L${document.documentElement.clientWidth} ${document.documentElement.clientHeight}`;
+let plane = document.querySelector('.plane');
+// let pathCoords = `M0 0 500 200 600 700 L${document.documentElement.clientWidth} ${document.documentElement.clientHeight}`;
+pathCoords = `M0 0 500 200  L600 700`;
+
 
 path.setAttribute('d', pathCoords);
 path.setAttribute('fill', 'none');
 path.setAttribute('stroke', '#000000');
 path.setAttribute('stroke-width', 2);
 path.setAttribute('id', 'path');
-
+path.setAttribute('d', pathCoords);
+// plane.style.offsetPath = `path(${pathCoords})`;
 document.getElementsByTagName('svg')[0].appendChild(path);
 
-console.log(path)
 
